587e7736b80ccaa1b2bbfb95126d0b7351ca6e5b
zemar-saad <152655678+zemar-saad@users.noreply.github.com>
2024-11-22 21:44:27 +0000

    Update grayScaleImage.c

--- grayScaleImage.c
+++ grayScaleImage.c
@@ -1,12 +1,12 @@
 #include "config/grayScaleImage.h"
 // Parameters:
-//   w: width of the image
-//   h: height of the image
+//   w: width of the image
+//   h: height of the image
 // Return value
-//   grayScale size of the image Bitmap (in byte)
+//   grayScale size of the image Bitmap (in byte)
 float grayScaleImage(int w, int h) {
-   // YOUR CODE HERE - BEGIN
-
-   // YOUR CODE HERE - END
-   return 0;
+    // YOUR CODE HERE - BEGIN
+   // Calculer la taille totale de l'image en octets
+   float size = w *h;
+   return size;
 }
