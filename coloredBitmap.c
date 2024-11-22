148ad5e9258db7ce42cc13f8be421caffa55a318
zemar-saad <152655678+zemar-saad@users.noreply.github.com>
2024-11-22 21:44:47 +0000

    Update coloredBitmap.c

--- coloredBitmap.c
+++ coloredBitmap.c
@@ -1,14 +1,27 @@
 #include <string.h>
 #include "config/coloredBitmap.h"
 // Parameters:
-//   w: width of the image
-//   h: height of the image
-//   u: Unit of the output value. It could be 'bt' bits, 'ko' kilobits, 'mo' megabits, 'go' gigabits
+//   w: width of the image
+//   h: height of the image
+//   u: Unit of the output value. It could be 'bt' bits, 'ko' kilobits, 'mo' megabits, 'go' gigabits
 // Return value
-//   colored image size Bitmap (based on the unit passed parametter)
+//   colored image size Bitmap (based on the unit passed parametter)
 float coloredBitmap(int w, int h, char* unit) {
-   // YOUR CODE HERE - BEGIN
+   // YOUR CODE HERE - BEGIN
+// Taille de base en bits (3 octets par pixel)
+    float colorsize = w * h * 3 ;
 
-   // YOUR CODE HERE - END
-   return 0;
+    // Conversion en fonction de l'unité
+    if (strcmp(unit, "bt") == 0) {
+        return colorsize*8; // Taille en bits
+    } else if (strcmp(unit, "ko") == 0) {
+        return colorsize / 1024; // Taille en kilobits
+    } else if (strcmp(unit, "mo") == 0) {
+        return (colorsize /1024) / 1024; // Taille en mégabits
+    } else if (strcmp(unit, "go") == 0) {
+        return ((colorsize /1024) / 1024) / 1024; // Taille en gigabits
+    }else 
+    return colorsize;
+   // YOUR CODE HERE - END
+   return 0;
 }
